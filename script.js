35cce5a82389c5ca179efe3b04520a43686cde40
harounian <joeyharounian@gmail.com>
2025-07-30 11:03:03 -0400

    added new porjects

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 const navbarLinks = document.querySelectorAll('.navbar-link-text');
 
-const writeAnimated = async (link, navLink) => {
-    for (let i = 0; i < link.length; i++) {
-        await new Promise((resolve) => setTimeout(resolve, (5))); //for typing animation: await new Promise((resolve) => setTimeout(resolve, (9*((Math.random() * 5)+1)))); // 25 --> increase/decrease speed
-        navLink.textContent += link.charAt(i);
-    }
-}
-
-(async () => {
-    await writeAnimated('Projects', navbarLinks[0]);
-    await writeAnimated('Publications', navbarLinks[1]);
-})();
+// Set navbar link text directly without animation
+navbarLinks[0].textContent = 'Projects';
+navbarLinks[1].textContent = 'Publications';
